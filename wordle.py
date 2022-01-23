0e4074aaab4e0b07c7098eda31fed62ca9814321
Connor Colabella <43245845+redowul@users.noreply.github.com>
2022-01-23 13:43:26 -0500

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -8,21 +8,21 @@ for word in list(set):
 print("This is a tool to filter out letters for the web game 'Wordle,' found here: https://www.powerlanguage.co.uk/wordle/")
 print("Using this tool is most certainly cheating!\n")
 while(True):
-    filter_list = input("Enter letters to filter out separated by commas (e.g. [a,b,c,d]):\n").split(",")
+    filter_list = input("Enter letters to filter out separated by commas (e.g. [a,b,c,d]):\n").lower().split(",")
     for letter in filter_list:
         if(letter.isalpha()):
             for word in list(set):
                 if letter in word:
                     set.remove(word)
         
-    filter_list = input("Enter KNOWN letters with KNOWN spaces with '?' acting as placeholder for UNKNOWN letters (e.g. '?pp??' for what is likely 'apple'):\n")
+    filter_list = input("Enter KNOWN letters with KNOWN spaces with '?' acting as placeholder for UNKNOWN letters separated by commas (e.g. '?,p,p,?,?' for what is likely 'apple'):\n").lower().split(",")
     for index, letter in enumerate(filter_list):
         if(letter.isalpha()):
             for word in list(set):
                 if word[index] != letter:
                     set.remove(word)
 
-    filter_list = input("Enter KNOWN letters with UNKNOWN spaces separated by commas (e.g. 'a,l,e'):\n").split(",")
+    filter_list = input("Enter KNOWN letters with UNKNOWN spaces separated by commas (e.g. 'a,l,e'):\n").lower().split(",")
     for letter in filter_list:
         if(letter.isalpha()):
             for word in list(set):
