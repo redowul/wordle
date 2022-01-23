c3f1cecc6ac74eae3214a78be2ecf774b3ada48e
Connor Colabella <43245845+redowul@users.noreply.github.com>
2022-01-23 15:46:13 -0500

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,9 +1,7 @@
-from english_words import english_words_lower_alpha_set
-
-set = english_words_lower_alpha_set
-for word in list(set):
-    if len(word) < 5 or len(word) > 5:
-        set.remove(word)
+set = []
+with open('words.txt') as file:
+    for line in file:
+        set.append(line.rstrip())
 
 print("This is a tool to filter out letters for the web game 'Wordle,' found here: https://www.powerlanguage.co.uk/wordle/")
 print("Using this tool is most certainly cheating!\n")
@@ -15,18 +13,18 @@ while(True):
                 if letter in word:
                     set.remove(word)
         
-    filter_list = input("Enter KNOWN letters with KNOWN spaces with '?' acting as placeholder for UNKNOWN letters separated by commas (e.g. '?,p,p,?,?' for what is likely 'apple'):\n").lower().split(",")
+    filter_list = input("Enter KNOWN letters with CORRECT placement with '?' acting as a placeholder for UNKNOWN letters separated by commas (e.g. '?,p,p,?,?' for what is likely 'apple'):\n").lower().split(",")
     for index, letter in enumerate(filter_list):
         if(letter.isalpha()):
             for word in list(set):
                 if word[index] != letter:
                     set.remove(word)
 
-    filter_list = input("Enter KNOWN letters with UNKNOWN spaces separated by commas (e.g. 'a,l,e'):\n").lower().split(",")
-    for letter in filter_list:
+    filter_list = input("Enter KNOWN letters with INCORRECT placement with '?' acting as a placeholder for UNKNOWN letters separated by commas (e.g. 'a,l,e'):\n").lower().split(",")
+    for index, letter in enumerate(filter_list):
         if(letter.isalpha()):
             for word in list(set):
-                if letter not in word:
+                if letter not in word or word[index] == letter:
                     set.remove(word)
 
     print("Remaining words:\n")
